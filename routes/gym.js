70d87b11ed14d70777967fed66c2e0306bd99106
Kendra Lyndon <kendralyndon@gmail.com>
2016-07-21 19:22:54 -0600

    user can battle pokemon and see who wins

--- routes/gym.js
+++ routes/gym.js
@@ -7,11 +7,20 @@ function Pokemon(){
 }
 
 router.get('/', function(req, res, next) {
+  var p1 = Number(req.cookies.p1);
+  var p2 = Number(req.cookies.p2);
+  var poke1;
+  var poke2;
   Pokemon().select().then(function(pokemon){
-    var p1 = Number(req.cookies.p1);
-    var p2 = Number(req.cookies.p2);
-    res.render('gym/index', {pokemon:pokemon, p1:p1, p2:p2});
-  })
+    pokemon.forEach(function(poke){
+      if(poke.id === p1){
+        poke1 = poke;
+      } else if(poke.id === p2){
+        poke2 = poke;
+      }
+    });
+    res.render('gym/index', {pokemon:pokemon, poke1:poke1, poke2:poke2, p1:p1, p2:p2});
+  });
 });
 
 module.exports = router;
